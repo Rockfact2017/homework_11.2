c9c54cddf815ae89617d120be7dcd1b66ed4ccf0
Rockfact2017 <160278344+Rockfact2017@users.noreply.github.com>
2024-03-05 19:50:31 +0300

    Added new function

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def capitalize(string):
     return string.upper()
+
+def capitalize_frst_lttr(string):
+    '''
+    делает заглавными первые буквы каждого слова в строке, поступившей на вход функции.
+    '''
+    return "".join(word.capitalize() for word in string.split())
